58fdba70e68ac4e24382808d86f02a1733592240
gitMaristela <maristela33781197@edu.df.senac.br>
2025-06-05 22:20:50 -0300

    Configura Whitenoise para servir arquivos estáticos

--- gerenciador/settings.py
+++ gerenciador/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Middleware for serving static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
